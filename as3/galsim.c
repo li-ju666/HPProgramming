2d5fa16b04cffbf067e95b9c9e2a49d2fddcea68
liju666 <li.ju.0122@student.uu.se>
2020-02-16 10:18:37 +0000

    firstversion

--- as3/galsim.c
+++ as3/galsim.c
@@ -24,7 +24,7 @@ int main(int argc, char* argv[]){
     int pnum = atoi(argv[1]), nsteps = atoi(argv[3]); 
     float delta_t = atof(argv[4]); 
     char graphic = atoi(argv[5]); 
-    particle part_ary[pnum]; 
+    particle* part_ary = malloc(sizeof(particle)*pnum); 
     
     // Read particle data from input file to particle information array
     FILE* fp = fopen(argv[2], "r"); 
@@ -40,45 +40,52 @@ int main(int argc, char* argv[]){
 	printf("Error: Input file damaged. \n"); 
 	return 1; 
     }
-    
-    /* for(int i=0; i<pnum; i++){ */
-    /* printf("Particle %d: \n", i); */ 
-    /* printf("%f, %f, %f, %f, %f, %f. \n", part_ary[i].position_x, */ 
-	    /* part_ary[i].position_y, part_ary[i].mass, part_ary[i].velocity_x, */ 
-	    /* part_ary[i].velocity_y,part_ary[i].brightness); */ 
-    /* } */
+
+#if 1
+    for(int i=0; i<pnum; i++){
+    printf("Particle %d: \n", i); 
+    printf("%f, %f, %f, %f, %f, %f. \n", part_ary[i].position_x, 
+	    part_ary[i].position_y, part_ary[i].mass, part_ary[i].velocity_x, 
+	    part_ary[i].velocity_y,part_ary[i].brightness); 
+    }
+#endif
+
 
     // Create a matrix(sequence) to store forces between each two particles and initialize all default value as 0
     vector2d* force_mat = (vector2d*)calloc(pnum*pnum, sizeof(vector2d)); 
     for(int step=0; step<nsteps; step++){
+	printf("Now is step: %d. \n", step); 
 	int i, j; 
 	double rij, temp; 
+#if 1
 	for(i=1; i<pnum; i++){
 	    for(j=0; j<i; j++){
 		
 		// Calculate the value of rij
 		rij = sqrt(pow((part_ary[i].position_x - part_ary[j].position_x), 2) + 
 			   pow((part_ary[i].position_y - part_ary[j].position_y), 2)); 
-		
+		/* printf("Now rij is: %f. \n", rij); */ 		
 		// Calculate the value of non-vector part
 		temp = -CONST_G * part_ary[i].mass * part_ary[j].mass / pow((rij + EPSILON), 3); 
-		
+		/* printf("Now const is: %f. \n", temp); */ 
 		// Store two coordinates of force vector into force array
 		force_mat[i*pnum+j].x = temp * (part_ary[i].position_x - part_ary[j].position_x); 
 		force_mat[i*pnum+j].y = temp * (part_ary[i].position_y - part_ary[j].position_y); 
 	    }
 	}
-
-	/* for(i=0; i<pnum; i++){ */
-	    /* for(j=0; j<pnum; j++){ */
-		/* printf("(%f, %f) ", force_mat[i*pnum+j].x, force_mat[i*pnum+j].y); */ 
-	    /* } */
-	    /* printf("\n"); */ 
-	/* } */
+	if(step==nsteps-1){
+	for(i=0; i<pnum; i++){
+	    for(j=0; j<pnum; j++){
+		printf("(%f, %f) ", force_mat[i*pnum+j].x, force_mat[i*pnum+j].y); 
+	    }
+	    printf("\n"); }}
 	
+#endif
+
+#if 1	
 	// Update attributes of particles in part_ary
 	vector2d acce; 
-	for(i=0; i<pnum; i++){
+	for(int i=0; i<pnum; i++){
 	    // Calculate the acceleration sum of particle i exerted by other particles
 	    acce.x = 0;
 	    acce.y = 0;  
@@ -104,12 +111,25 @@ int main(int argc, char* argv[]){
 	    part_ary[i].position_x += part_ary[i].velocity_x * delta_t; 
 	    part_ary[i].position_y += part_ary[i].velocity_y * delta_t; 
 	}
+#endif
     }
-    fp = fopen("result", "w"); 
+#if 1
+    for(int i=0; i<pnum; i++){
+    printf("Particle %d: \n", i); 
+    printf("%f, %f, %f, %f, %f, %f. \n", part_ary[i].position_x, 
+	    part_ary[i].position_y, part_ary[i].mass, part_ary[i].velocity_x, 
+	    part_ary[i].velocity_y,part_ary[i].brightness); 
+    }
+#endif
+
+#if 1
+    fp = fopen("result.gal", "w"); 
     if(fwrite(part_ary, pnum, sizeof(particle), fp) == 0){
 	printf("Error: Write file failed! \n"); 
 	return 1; 	
      }; 
     fclose(fp); 
-    free(part_ary); 
+    free(part_ary);
+    free(force_mat);  
+#endif
 }
