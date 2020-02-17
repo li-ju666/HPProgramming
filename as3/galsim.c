ae42d32188df56a12e8ddc212e36cf120d6539f2
liju666 <li.ju.0122@student.uu.se>
2020-02-17 21:43:42 +0000

    shell_for_batch_test

--- as3/galsim.c
+++ as3/galsim.c
@@ -37,11 +37,12 @@ int main(int argc, char* argv[]){
 	fclose(fp);
     }
     else{
-	printf("Error: Input file damaged. \n"); 
+	printf("Error: Invalid input particle numbers. \n");
+	fclose(fp); 	
 	return 1; 
     }
 
-#if 1
+#if 0
     for(int i=0; i<pnum; i++){
     printf("Particle %d: \n", i); 
     printf("%f, %f, %f, %f, %f, %f. \n", part_ary[i].position_x, 
@@ -54,50 +55,81 @@ int main(int argc, char* argv[]){
     // Create a matrix(sequence) to store forces between each two particles and initialize all default value as 0
     vector2d* force_mat = (vector2d*)calloc(pnum*pnum, sizeof(vector2d)); 
     for(int step=0; step<nsteps; step++){
-	printf("Now is step: %d. \n", step); 
+	/* printf("Now is step: %d. \n", step); */ 
 	int i, j; 
-	double rij, temp; 
+	double rij, temp_coeffi, x_cord, y_cord; 
 #if 1
 	for(i=1; i<pnum; i++){
 	    for(j=0; j<i; j++){
 		
 		// Calculate the value of rij
-		rij = sqrt(pow((part_ary[i].position_x - part_ary[j].position_x), 2) + 
-			   pow((part_ary[i].position_y - part_ary[j].position_y), 2)); 
+		x_cord = (part_ary[i].position_x - part_ary[j].position_x) * (part_ary[i].position_x - part_ary[j].position_x);
+		y_cord = (part_ary[i].position_y - part_ary[j].position_y) * (part_ary[i].position_y - part_ary[j].position_y); 
+		rij = sqrt(x_cord + y_cord); 
+
+		/* rij = sqrt(pow((part_ary[i].position_x - part_ary[j].position_x), 2) + */ 
+		/* 	   pow((part_ary[i].position_y - part_ary[j].position_y), 2)); */ 
 		/* printf("Now rij is: %f. \n", rij); */ 		
 		// Calculate the value of non-vector part
-		temp = -CONST_G * part_ary[i].mass * part_ary[j].mass / pow((rij + EPSILON), 3); 
+		
+		/* temp = -CONST_G * part_ary[i].mass * part_ary[j].mass / pow((rij + EPSILON), 3); */ 
+		
+		temp_coeffi = -CONST_G * part_ary[i].mass * part_ary[j].mass / (rij * (x_cord + y_cord) + 3 * rij * EPSILON * 
+		    EPSILON + 3 * (x_cord + y_cord) * EPSILON + EPSILON * EPSILON * EPSILON); 
 		/* printf("Now const is: %f. \n", temp); */ 
 		// Store two coordinates of force vector into force array
-		force_mat[i*pnum+j].x = temp * (part_ary[i].position_x - part_ary[j].position_x); 
-		force_mat[i*pnum+j].y = temp * (part_ary[i].position_y - part_ary[j].position_y); 
+		force_mat[i*pnum+j].x = temp_coeffi * (part_ary[i].position_x - part_ary[j].position_x); 
+		force_mat[i*pnum+j].y = temp_coeffi * (part_ary[i].position_y - part_ary[j].position_y); 
 	    }
 	}
-	if(step==nsteps-1){
-	for(i=0; i<pnum; i++){
-	    for(j=0; j<pnum; j++){
-		printf("(%f, %f) ", force_mat[i*pnum+j].x, force_mat[i*pnum+j].y); 
-	    }
-	    printf("\n"); }}
+	/* if(step==nsteps-1){ */
+	/* for(i=0; i<pnum; i++){ */
+	/*     for(j=0; j<pnum; j++){ */
+	/* 	printf("(%f, %f) ", force_mat[i*pnum+j].x, force_mat[i*pnum+j].y); */ 
+	/*     } */
+	/*     printf("\n"); }} */
 	
 #endif
 
-#if 1	
+#if 1
 	// Update attributes of particles in part_ary
-	vector2d acce; 
+	vector2d acce; //, error, temp; 
 	for(int i=0; i<pnum; i++){
 	    // Calculate the acceleration sum of particle i exerted by other particles
 	    acce.x = 0;
-	    acce.y = 0;  
+	    acce.y = 0; 
+	    /* error.x = 0; */ 
+	    /* error.y = 0; */ 
+	    /* temp.x = 0; */ 
+	    /* temp.y = 0; */ 
 	    // Forces j exerted to i are added positively
 	    for(j=0; j<i; j++){
+		/* temp.x = acce.x + force_mat[i*pnum+j].x - error.x; */ 
+		/* error.x = temp.x - acce.x - force_mat[i*pnum+j].x; */ 
+		/* acce.x = temp.x; */ 
+		
+		/* temp.y = acce.y + force_mat[i*pnum+j].y; */ 
+		/* error.y = temp.y - acce.y - force_mat[i*pnum+j].y; */ 
+		/* acce.y = temp.y; */ 
+
 		acce.x += force_mat[i*pnum+j].x; 
 		acce.y += force_mat[i*pnum+j].y; 
 	    }
 	    // Forces i exerted to j are added negatively, according to Newton's 3rd Law
+	    /* error.x = 0; */ 
+	    /* error.y = 0; */ 
 	    for(j=i+1; j<pnum; j++){
 		acce.x -= force_mat[j*pnum+i].x; 
 		acce.y -= force_mat[j*pnum+i].y; 
+
+		/* temp.x = acce.x - force_mat[i*pnum+j].x - error.x; */ 
+		/* error.x = temp.x - acce.x + force_mat[i*pnum+j].x; */ 
+		/* acce.x = temp.x; */ 
+		
+		/* temp.y = acce.y - force_mat[i*pnum+j].y; */ 
+		/* error.y = temp.y - acce.y + force_mat[i*pnum+j].y; */ 
+		/* acce.y = temp.y; */ 
+
 	    }
 	    // Acceleration is calculated by Newton's 2nd Law
 	    acce.x = acce.x/part_ary[i].mass; 
@@ -113,7 +145,7 @@ int main(int argc, char* argv[]){
 	}
 #endif
     }
-#if 1
+#if 0
     for(int i=0; i<pnum; i++){
     printf("Particle %d: \n", i); 
     printf("%f, %f, %f, %f, %f, %f. \n", part_ary[i].position_x, 
---
+++ as3/test.sh
@@ -0,0 +1,23 @@
+# rm result.gal
+# gcc -o galsim galsim.c -lm -O3
+# ./galsim 100 ellipse_N_00100.gal 200 0.00001 0
+# ./compare 100 result.gal ellipse_N_00100_after200steps.gal
+# time ./galsim 100 ellipse_N_00100.gal 200 0.00001 0
+
+#!bin/bash
+gcc -o galsim -O3 galsim.c -lm
+folder_ref="/home/sariel/HPP/as3/Assignment3/ref_output_data/"
+folder_input="/home/sariel/HPP/as3/Assignment3/input_data/"
+tail=".gal"
+for file in $(ls ${folder_ref})
+do
+    echo ${file:10:5} ${folder_input}${file:0:15}${tail} ${file:21:3} >> report.log
+    ./galsim ${file:10:5} ${folder_input}${file:0:15}${tail} ${file:21:3} 0.00001 0
+    ./compare ${file:10:5} result.gal ${folder_ref}${file} >> report.log
+    for i in {1..10}
+    do
+	echo ${i}
+	time ./galsim ${file:10:5} ${folder_input}${file:0:15}${tail} ${file:21:3} 0.00001 0 >> report.log
+    done
+
+done
